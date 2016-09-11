c353a42f3b80f699d294dc34b10487810035f961
Jason Morita <jason.morita@globality.com>
2016-09-11 12:45:23 -0700

    Rename to Character

---
+++ client/src/components/character/Character.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getGraph } from '../../actions/character.js';
+import get from 'lodash.get';
+
+const query = `
+query Person {
+    person(id: "1") {
+        name
+        url
+        vehicles {
+            name
+        }
+        films {
+            title
+        }
+        species {
+            name
+        }
+        entity {
+            image
+        }
+    }
+}
+`;
+
+let Character = React.createClass({
+    componentDidMount() {
+        this.props.dispatch(getGraph(query));
+    },
+    render() {
+        let fetching = this.props.store.character.fetching;
+        let name = get(this.props.store.character, 'character.name');
+
+        return (
+            <div>
+                <p>Fetching: {fetching}</p>
+                <h3>{ name }</h3>
+                <h3>Vehicles:</h3>
+                <ul>
+                </ul>
+            </div>
+        )
+    }
+});
+
+const mapStateToProps = (state) => {
+    return {
+        store: state
+    }
+};
+
+export const CharacterContainer = connect(
+    mapStateToProps
+)(Character);
